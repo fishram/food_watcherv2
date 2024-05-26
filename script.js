776ff2db2dafeb48917488f6fc42fda9a0120637
Fisher Ramsey <fishram@unc.edu>
2024-05-25 20:01:20 -0400

    add food form styled

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
 console.log("Dom Loaded!")
 
 // HTML button elements
-const addFoodButton = document.querySelector('#add-food-button');
+const addFoodButton = document.querySelector('#add-food-button-id');
 const homeScreen = document.querySelector("#home-screen-id");
 const addFoodScreen = document.querySelector('#add-food-screen-id');
+const locationSelect = document.querySelector('#locations');
+
+// Location list
+let locationList = ["Fridge", "Freezer", "Pantry"];
 
 // button1 click event listener to apply function
 addFoodButton.addEventListener('click', () => {
@@ -12,5 +16,16 @@ addFoodButton.addEventListener('click', () => {
     homeScreen.style.display = "none";
     addFoodScreen.style.display = "flex";
 });
+
+function addLocations(){
+    locationList.forEach((location) => {
+        const newOption = document.createElement('option');
+        newOption.text = location;
+        newOption.value = location.toLowerCase().replace(/\s+/g, '-');
+        locationSelect.add(newOption);
+    });
+}
+
+addLocations();
 
 });
