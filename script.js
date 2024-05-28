e2e3c72a0fb30985f1b76a5d2f08f19eb74b9ee7
FisherRamsey <fisher.ramsey27@gmail.com>
2024-05-27 21:29:32 -0400

    Cancel button functionality

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
 document.addEventListener('DOMContentLoaded', () => {
-console.log("Dom Loaded!")
+    console.log("DOM Loaded!");
 
-// HTML button elements
-const addFoodButton = document.querySelector('#add-food-button-id');
-const homeScreen = document.querySelector("#home-screen-id");
-const addFoodScreen = document.querySelector('#add-food-screen-id');
-const locationSelect = document.querySelector('#locations');
-const cancelButton = document.querySelector('#food-screen-cancel-button');
+    // Screens
+    const homeScreen = document.querySelector("#home-screen-id");
+    const addFoodScreen = document.querySelector('#add-food-screen-id');
+    const foodConfirmScreen = document.querySelector('#confirm-food-screen-id');
 
-// Location list
-let locationList = ["Fridge", "Freezer", "Pantry"];
+    // Buttons
+    const cancelButton = document.querySelector('#food-screen-cancel-button');
+    const addFoodButton = document.querySelector('#add-food-button-id');
 
-// button1 click event listener to apply function
-addFoodButton.addEventListener('click', () => {
-    console.log("Add Food Button Clicked!");
-    homeScreen.style.display = "none";
-    addFoodScreen.style.display = "flex";
-});
+    // Forms
+    const addFoodForm = document.querySelector('#food-form-id');
+    const locationSelect = document.querySelector('#locations');
 
-cancelButton.addEventListener
 
-function addLocations(){
-    locationList.forEach((location) => {
-        const newOption = document.createElement('option');
-        newOption.text = location;
-        newOption.value = location.toLowerCase().replace(/\s+/g, '-');
-        locationSelect.add(newOption);
+    // Arrays
+    let locationList = ["Fridge", "Freezer", "Pantry"];
+    let foodList = [];
+
+    addFoodButton.addEventListener('click', () => {
+        console.log("Add Food Button Clicked!");
+        homeScreen.style.display = "none";
+        addFoodScreen.style.display = "flex";
     });
-}
 
-addLocations();
+    cancelButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log("Cancel Button Clicked!");
+        addFoodScreen.style.display = "none";
+        homeScreen.style.display = "flex";
+    });
 
+    function addLocations() {
+        locationList.forEach((location) => {
+            const newOption = document.createElement('option');
+            newOption.text = location;
+            newOption.value = location.toLowerCase().replace(/\s+/g, '-');
+            locationSelect.add(newOption);
+        });
+    };
+
+    addFoodForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        let foodName = document.querySelector('#foodName').value;
+        let location = document.querySelector('#locations').value;
+        let quantity = document.querySelector('#quantity').value;
+        let expirationDate = document.querySelector('#expirationDate').value;
+
+        const foodItem = {
+            foodname: foodName,
+            location: location,
+            quantity: quantity,
+            expirationDate: expirationDate
+        };
+
+        foodList.push(foodItem);
+        addFoodForm.reset();
+
+        addFoodScreen.style.display = "none";
+        foodConfirmScreen.style.display = 'flex';
+
+    });
+
+    addLocations();
 });
